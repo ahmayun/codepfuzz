f739caa5335d79e746fa70e1fe1d7d8e952e144d
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-08-13 11:12:00 -0700

    Fix wordcount name

--- src/main/scala/examples/benchmarks/WordCount.scala
+++ src/main/scala/examples/benchmarks/WordCount.scala
@@ -8,7 +8,7 @@ object WordCount extends Serializable {
     val sparkConf = new SparkConf()
     if (args.length < 2) throw new IllegalArgumentException("Program was called with too few args")
 //    sparkConf.setMaster(args(1))
-    sparkConf.setAppName("WordCount Monitored")
+    sparkConf.setAppName("WordCount")
     val ctx = SparkContext.getOrCreate(sparkConf)
 //    ctx.setLogLevel("ERROR")
     val ds = ctx.textFile(args(0)).map(_.split("\\s"))
